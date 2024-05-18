0480fc58087336920d73b41b6bd38c120193fc1b
silverDk-eng <108482364+silverDk-eng@users.noreply.github.com>
2024-05-19 03:12:29 +0900

    LED 추가, 동작은 하나, debug 모드 및 보드인식이 stlink에서 동작하지 않음.

---
+++ STM32F103C8Tx_BluePill/Core/Inc/stm32c8t6_bluepill_board.h
@@ -0,0 +1,10 @@
+#ifndef _STM32C8T6_BLUEPILL_BOARD_H_
+//#include "stm32c8t6_bluepill_board.h"
+#define _STM32C8T6_BLUEPILL_BOARD_H_
+
+#include "stm32f1xx_hal.h"
+
+#define LED_Pin GPIO_PIN_13
+#define LED_GPIO_Port GPIOC
+
+#endif
